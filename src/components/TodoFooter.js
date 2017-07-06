a49be5ee4c529660d20e76bb5eab6e5df8dc58f8
zhufengnodejs <zhufengnodejs@126.com>
2017-07-06 15:46:40 +0800

    8.实现过滤

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -2,8 +2,18 @@ import React,{Component} from 'react';
 export default class TodoFooter extends Component{
     render(){
         return (
-            <div>
-              TodoFooter
+            <div className="row">
+              <div className="col-sm-4">
+                <span style={{lineHeight:'34px'}}>你还有件待办事项</span>
+              </div>
+              <div className="col-sm-5">
+                 <button className="btn btn-default">全部</button>
+                  <button style={{marginLeft:5}} className="btn btn-default">未完成</button>
+                  <button style={{marginLeft:5}} className="btn btn-default">已完成</button>
+              </div>
+              <div className="col-sm-3">
+                 <button className="btn btn-danger">清除已完成</button>
+              </div>
             </div>
         )
     }
