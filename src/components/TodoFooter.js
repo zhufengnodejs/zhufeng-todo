30ae2d61d09d023a5071ada381473c7e97336872
zhufengnodejs <zhufengnodejs@126.com>
2017-07-06 15:59:35 +0800

    9.删除所有已完成事项

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,20 +1,34 @@
 import React,{Component} from 'react';
-export default class TodoFooter extends Component{
+import {connect} from 'react-redux';
+class TodoFooter extends Component{
     render(){
         return (
             <div className="row">
               <div className="col-sm-4">
-                <span style={{lineHeight:'34px'}}>你还有件待办事项</span>
+                {
+                  this.props.activeCount>0?<span style={{lineHeight:'34px'}}>你还有{this.props.activeCount}件待办事项</span>:null
+                }
               </div>
               <div className="col-sm-5">
                  <button className="btn btn-default">全部</button>
-                  <button style={{marginLeft:5}} className="btn btn-default">未完成</button>
-                  <button style={{marginLeft:5}} className="btn btn-default">已完成</button>
+                  <button
+                    style={{marginLeft:5}}
+                    className="btn btn-default">未完成</button>
+                  <button
+                    style={{marginLeft:5}}
+                    className="btn btn-default">已完成</button>
               </div>
               <div className="col-sm-3">
-                 <button className="btn btn-danger">清除已完成</button>
+                {this.props.completedCount>0?<button className="btn btn-danger">清除已完成</button>:null}
               </div>
             </div>
         )
     }
 }
+
+export default connect(
+ state=>({
+   activeCount:state.todos.filter(item=>!item.completed).length,
+   completedCount:state.todos.filter(item=>item.completed).length
+ })
+)(TodoFooter);
--- src/store/action-types.js
+++ src/store/action-types.js
@@ -4,6 +4,8 @@ export const ADD_TODO = 'ADD_TODO';
 export const DEL_TODO = 'DEL_TODO';
 //切换一个待办事项的状态
 export const TOGGLE_TODO = 'TOGGLE_TODO';
+//删除所有已完成的待办事项
+export const DELETE_ALL_COMPLETED = 'DELETE_ALL_COMPLETED';
 
 //修改过滤类型 all(显示全部) active(显示未完成) completed(只显示已完成的)
 export const CHANGE_FILTER = 'CHANGE_FILTER';
